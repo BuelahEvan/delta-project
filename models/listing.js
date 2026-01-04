f2bfeaf4edbcd1fe72b1ab7d1076aa46caab21db
Buelah <buelahevangiline64@gmail.com>
2026-01-04 17:10:30 +0530

    Fix listing delete middleware error

--- models/listing.js
+++ models/listing.js
@@ -1,12 +1,37 @@
 
 //with a cottage listing all its reviews also can be deleted
-listingSchema.post("findOneandDelete",async(listing)=>{
-    if(listing){
-           await Review.deleteMany({_id:{$in: listing.reviews}});
-    }
- 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review"); // Ensure Review is imported
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        url: String,
+        filename: String,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
 });
 
 
-const Listing=mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
+});
+
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
