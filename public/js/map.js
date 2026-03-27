d5ccc570b2e09fecefe7fc09b65260dffa9dd96e
Buelah <buelahevangiline64@gmail.com>
2026-03-27 17:35:45 +0530

    for map error

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,10 @@
 
   
-    mapboxgl.accessToken = mapToken;
+    mapboxgl.accessToken = window.mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style:"mapbox://styles/mapbox/streets-v12", //style url
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: window.listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9// starting zoom
     });
 
@@ -18,11 +18,11 @@
   // Create a default Marker and add it to the map.
 
     const marker = new mapboxgl.Marker({color :"red"})
-        .setLngLat(listing.geometry.coordinates) //listing.geometry.coordinates
+        .setLngLat(window.listing.geometry.coordinates) //listing.geometry.coordinates
         //code copied from https://docs.mapbox.com/mapbox-gl-js/api/markers/ in popup example last lines
         .setPopup( new mapboxgl.Popup({offset: 25})
     .setHTML(
-        `<h4>${listing.location}</h4><p>Exact location will be  provided after booking</p>`
+        `<h4>${window.listing.location}</h4><p>Exact location will be  provided after booking</p>`
     )
   )
         .addTo(map);
